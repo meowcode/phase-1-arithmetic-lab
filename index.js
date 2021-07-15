046e19803e6e4fcce18c3804dd54934951fa7ea3
Yuyuhita Guzman <yuyuhita.guzman@gmail.com>
2021-07-15 18:47:59 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+let num1=31, num2=2
+const multiply=(num1*num2);
+
+let random = Math.floor(Math.random() * 10);
+
+let num3=48, num4=12
+const mod=(num3/num4);
+
+
+let max = Math.max(5,10,15,20);
